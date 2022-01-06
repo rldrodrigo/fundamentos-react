58683fef76f3307b8d5fe32c3ad2faa27486b83f
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2022-01-06 18:05:29 -0300

    Uso de Hooks useState para comunicação indireta

--- src/components/comunicacao/IndiretaFilho.jsx
+++ src/components/comunicacao/IndiretaFilho.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
 export default props => {
+    const cb = props.quandoClicar
+    const min = 50
+    const max = 70
+    const gerarIdade = () => parseInt(Math.random() * (max - min)) + min
+    const gerarNerd = () => Math.random() > 0.5
     return (
         <div>
             <div>
                 Filho
             </div>
-            <button onClick={_ => props.quandoClicar('João', 53, true)}> Fornecer informações</button>
+            <button onClick={_ => cb('João', gerarIdade(), gerarNerd())}> Fornecer informações</button>
         </div>
     )
 }
